956fa5883f046452adab9513f77da460f43df316
koladev <onaelmangabo@gmail.com>
2022-07-20 16:35:24 +0100

    ✨ add profiles endpoint

--- server/app.py
+++ server/app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask import Flask, jsonify, request
+
+from .client import ProxyCurlClient
 
 app = Flask(__name__)
+
+client = ProxyCurlClient()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/profiles/", methods=["POST"])
+def retrieve_profiles():
+    if "search" not in request.json:
+        return jsonify({"erorr": "Missing search parameter"}), 400
+    
+    data = client.get_profiles(request.json["search"])
+    
+    return jsonify(data), 200
---
+++ server/client.py
@@ -0,0 +1,12 @@
+import os
+
+PROXYCURL_API_KEY = os.environ.get('PROXY_API_KEY', None)
+
+
+class ProxyCurlClient:
+    
+    def __init__(self):
+        pass
+    
+    def get_profiles(keyword):
+        pass
